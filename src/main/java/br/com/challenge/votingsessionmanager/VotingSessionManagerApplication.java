962808727ca668c925a3bdd61de3c35033ab3f6c
Davi Souza Sande <dsouzasande@wsgc.com>
2022-10-17 18:29:07 -0700

    Project initial files

---
+++ src/main/java/br/com/challenge/votingsessionmanager/VotingSessionManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenge.votingsessionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSessionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSessionManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/challenge/votingsessionmanager/VotingSessionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge.votingsessionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSessionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
